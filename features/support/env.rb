91d54b3eea86030401eb6946579044026f8bed30
Dennis Günnewig <dg1@ratiodata.de>
2016-03-09 10:45:01 +0100

    Fix dependency

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,12 +5,9 @@ require 'simplecov'
 SimpleCov.command_name 'cucumber'
 SimpleCov.start
 
-require 'pry'
-require 'byebug'
-
 # Pull in all of the gems including those in the `test` group
-# require 'bundler'
-# Bundler.require :default, :test, :development
+require 'bundler'
+Bundler.require :default, :test, :development, :debug
 
 ENV['TEST'] = 'true'
 
